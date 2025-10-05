f1b32f992798321143393078999a9c4a69f6fe17
dkac14 <150403101+dkac14@users.noreply.github.com>
2025-10-05 01:33:17 -0500

    nose

--- tu-proyecto/vite.config.ts
+++ tu-proyecto/vite.config.ts
@@ -1,16 +1,37 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import type { UserConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  let build: UserConfig['build'], esbuild: UserConfig['esbuild'], define: UserConfig['define']
+
+  if (mode === 'development') {
+    build = {
+      minify: false,
+      rollupOptions: { output: { manualChunks: undefined } },
+    }
+    esbuild = { jsxDev: true, keepNames: true, minifyIdentifiers: false }
+    define = { 'process.env.NODE_ENV': '"development"', 'DEV': 'true' }
+  }
+
+  return {
+    plugins: [react()],
+    build,
+    esbuild,
+    define,
+    resolve: { alias: { '@': '/src' } },
+    optimizeDeps: { exclude: ['lucide-react'] },
+
+    // 👇👇👇  AÑADE ESTO
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8000/',
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+    // ☝️☝️☝️
+  }
+})
